89c190d97efb0b0c4b33fe14661d426c902f51e2
SEOJEONG <plant00park21@gmail.com>
2021-02-13 00:15:34 +0900

    basic CBV

--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, render
 from .models import Post
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 
 post_list = ListView.as_view(model=Post)
 
@@ -16,10 +16,13 @@ post_list = ListView.as_view(model=Post)
 #     })
 
 
-def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    response = HttpResponse()
-    response.write("Hello World")
-    return response
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'instagram/post_detail.html', {
+#         'post' :post,
+#     })
     
+post_detail = DetailView.as_view(model=Post)
+
 def archives_year(request, year):
     return HttpResponse(f"{year}년 archives")
